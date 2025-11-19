413c96e77bb3b385d7c845e599343a8183bf62ea
Vincent Van Bocxlaer <vvb@wiot-tomorrow.com>
2025-11-19 11:29:43 +0100

    Refactor `Termable` model to extend `MorphPivot`, add `term` relationship.

--- src/Models/Termable.php
+++ src/Models/Termable.php
@@ -2,15 +2,21 @@
 
 namespace Syndicate\Taxonomist\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class Termable extends Model
+class Termable extends MorphPivot
 {
     protected $table = 'termables';
 
     public function model(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function term(): BelongsTo
+    {
+        return $this->belongsTo(Term::class);
     }
 }
